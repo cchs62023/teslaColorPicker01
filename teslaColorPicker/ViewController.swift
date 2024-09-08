7fa8a7f7e50e1e39fb5a09850b7cde6d35b19f21
linda <monthttest970509@gmail.com>
2024-09-08 09:51:53 +0800

    Initial Commit

---
+++ teslaColorPicker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  teslaColorPicker
+//
+//  Created by 林家齊 on 2024/9/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
