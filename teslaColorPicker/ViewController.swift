f8bc558c813d2358d2eeda27b8e38e747a736831
linda <monthttest970509@gmail.com>
2024-09-08 16:46:47 +0800

    mask issue

--- teslaColorPicker/ViewController.swift
+++ teslaColorPicker/ViewController.swift
@@ -28,6 +28,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var carMask: UIImageView!
     
     
+    @IBOutlet weak var redText: UILabel!
+    
+    @IBOutlet weak var GreenText: UILabel!
+    
+    
+    @IBOutlet weak var BlueText: UILabel!
+    
+    
+    @IBOutlet weak var `switch`: UISegmentedControl!
+    
+    let pics = ["teslabk-0", "teslabk-1"]
+    var index = 0
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,35 +63,36 @@ class ViewController: UIViewController {
             // Do any additional setup after loading the view.
         }
         
-
+        
     }
-    
     
     
     @IBAction func changeColor(_ sender: Any) {
         let color = UIColor(red: CGFloat(redChange.value),
-                              green: CGFloat(greenChange.value),
-                              blue: CGFloat(blueChange.value),
-                              alpha: 1)
+                            green: CGFloat(greenChange.value),
+                            blue: CGFloat(blueChange.value),
+                            alpha: 1)
         carImage.backgroundColor = color
     }
-        
-    
-    
-    
-    
-    
     
     
     @IBAction func random(_ sender: Any) {
         redChange.value = Float.random(in: 0...1)
-              greenChange.value = Float.random(in: 0...1)
-              blueChange.value = Float.random(in: 0...1)
-              changeColor(self)
-    }
-    
+        greenChange.value = Float.random(in: 0...1)
+        blueChange.value = Float.random(in: 0...1)
+        changeColor(self)
+        
         
     }
     
     
     
+    @IBAction func segmentControlAction(_ sender: UISegmentedControl) {
+        index = sender.selectedSegmentIndex
+        let name = pics[index]
+        background.image = UIImage(named: name)
+        
+        
+    }
+    
+}
