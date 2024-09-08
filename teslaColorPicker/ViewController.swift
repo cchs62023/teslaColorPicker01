2bb584edaaf773b31fa382cf05c9bd715b3cd5a8
linda <monthttest970509@gmail.com>
2024-09-08 15:04:07 +0800

    add

--- teslaColorPicker/ViewController.swift
+++ teslaColorPicker/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var background: UIImageView!
+    
+    @IBOutlet weak var carImage: UIImageView!
+    
+    
+    @IBOutlet weak var redChange: UISlider!
+    
+    
+    @IBOutlet weak var greenChange: UISlider!
+    
+    
+    @IBOutlet weak var blueChange: UISlider!
+    
+    
+    @IBOutlet weak var carMask: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // 設置遮罩
+            if let maskImage = carMask.image?.cgImage {
+                let maskLayer = CALayer()
+                maskLayer.contents = maskImage
+                maskLayer.frame = carImage.bounds
+                carImage.layer.mask = maskLayer
+                carImage.layer.masksToBounds = true
+        
+        
+        
+        
         // Do any additional setup after loading the view.
     }
 
 
-}
 
+
+}
+}
